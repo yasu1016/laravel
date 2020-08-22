f8d7fddb53098d093f5a837a6a0803990aa1a585
Yasuyuki Fukuda <yfukuda1016@gmail.com>
2020-08-22 01:26:09 +0000

    kadai

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        
-        <title>プロファイル/クリエイト</title>
-    </head>
-    <body>
-        <h1>プロファイル/クリエイト</h1>
-    </body>
-</html>
+@extends('layouts.profile')
+
+@section('title','My プロフィール')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
