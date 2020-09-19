e45ed486d0e38370f8ada7bb36f9a319066a979d
Yasuyuki Fukuda <yfukuda1016@gmail.com>
2020-09-19 02:20:38 +0000

    kadai

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\News;
+use App\History;
+use Carbon\Carbon;
 
 class NewsController extends Controller
 {
@@ -63,20 +65,27 @@ class NewsController extends Controller
         $this->validate($request, News::$rules);
         $news = News::find($request->id);
         $news_form = $request->all();
-        if (isset($news_form['image'])) {
+        if ($request->remove == 'true') {
+            $news_form['image_path'] = null;
+        } elseif ($request->file('image')) {
             $path = $request->file('image')->store('public/image');
-            $news->image_path = basename($path);
-            unset($news_form['image']);
-        } elseif (isset($request->remove)) {
-            $news->image_path = null;
-            unset($news_form['remove']);
+            $news_form['image_path'] = basename($path);
+        } else {
+            $news_form['image_path'] = $news->image_path;
         }
         
         unset($news_form['_token']);
+        unset($news_form['image']);
+        unset($news_form['remove']);
         
         $news->fill($news_form)->save();
         
-        return redirect('admin/news');
+        $history = new History;
+        $history->news_id = $news->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
+        
+        return redirect('admin/news/');
     }
     
     public function delete(Request $request)
